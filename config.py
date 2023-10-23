8d5c88be7c4f04a8d882ffcc393344392ec43c60
huan-cdm <huan151009@163.com>
2023-10-23 22:37:22 +0800

    删除配置信息

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 #配置变更，需要重启服务才能生效。
 #fofa配置
-fofakey = "9741ff706e07eb9251fae1cefce030c8"
+fofakey = ""
 #fofa配置，批量检索查询最多条数
-batchfofanum = "100"
+batchfofanum = ""
 #批量检索休眠时间
-batchfofasleeptime = 2
+batchfofasleeptime = 
 #fofa邮箱配置
-fofaemail = "weakchicken@qq.com"
+fofaemail = ""
 
 #MySQL数据库配置
 dict = {
-    "ip":"127.0.0.1","username":"mysql","password":"zwroot123!@#","dbname":"CNVD","portnum": 3306
+    "ip":"","username":"","password":"","dbname":"","portnum": 3306
 }
 
 
 #shodan配置
-shodankey = "5wdqqD7mCbWQdehFqWhk5aKVK0OtwR0Z"
+shodankey = ""
 
 
 #信息收集平台web访问配置
@@ -35,21 +35,21 @@ monlinkconfig = {
 
 
 #Elasticsearch数据库配置
-esusername = "elastic"
-espassword = "okoHRTB6pJS3Ltw=XouX"
-esurl = "https://121.37.207.248:9200"
+esusername = ""
+espassword = ""
+esurl = ""
 
 
 #vulmap漏洞扫描器参数配置,XLSXz转换，sheet名称配置
 dir_vul_url = "urls"
 
 #arl资产探测平台登录方式和账号密码
-arlurl = "https://121.37.207.248:5003/login"
-arluserpass = "admin/zwroot123!@#"
+arlurl = ""
+arluserpass = ""
 
 #github监控平台登录方式和账号密码
-githuburl = "http://121.37.207.248:8001/"
-githubpass = "admin/zwroot123!@#"
+githuburl = ""
+githubpass = ""
 
 #配置系统启动是否开启debug模式
 # 0:关闭debug模式
